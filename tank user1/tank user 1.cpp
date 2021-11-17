21af1602b7f6b427dc273062f4382c388f51415d
seungdo1234 <wltmdeh12@naver.com>
2021-11-17 20:29:58 +0900

    탱크 1

--- tank user1/tank user 1.cpp
+++ tank user1/tank user 1.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
+#include <winsock2.h>
 #include<conio.h>
 #include<Windows.h>
 #include<ctime>
+#include <stdlib.h>
+#include <process.h>
+
+
+using namespace std;
 
 #define UP 72
 #define DOWN 80
@@ -9,14 +15,18 @@
 #define RIGHT 77
 #define STOP 115 
 #define SPACE 32
+#define PORT 23000 /*서버와 클라이언트간에 데이터를 주고 받을 포트번호*/
+#define BUFFER_SIZE  100 
 
-using namespace std;
+
+#pragma comment (lib, "ws2_32.lib")
+#pragma warning(disable:4996)
 
 
 class tank_p {
 public:
 	int map[10][10] = { 0, };
-	int tx = 0, ty = 8;
+	int coordinate[3] = { 8,0,2 };
 	virtual void move(void) = 0;
 	tank_p();
 };
@@ -45,62 +55,110 @@ tank_p::tank_p() {
 }
 class tank_c : public tank_p {
 private:
+	WSADATA wsdata;
+	SOCKADDR_IN serverAddress;
+	SOCKET clientSocket;
 	int key = 0;
 	int tmp = 0;
 public:
+	void Cli_St();
 	void move(void);
 	void print();
 	void set();
+	void con();
 };
+
+void tank_c::Cli_St() {
+	int d;
+	/*WS_32.DLL 을 초기화 */
+	if (WSAStartup(MAKEWORD(2, 2), &wsdata) != 0) {
+		cout << "WS2_32.DLL 을 초기화 하는데 실패했습니다. " << endl;
+		return;
+	}
+	cout << "WS_32.DLL 을 초기화 하는데 성공 했습니다 " << endl;
+	/*socket 함수를 이용해서 clientSocket을 생성 */
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	ZeroMemory(&serverAddress, sizeof(serverAddress));
+	serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+	/*주소의 종류 대입*/
+	serverAddress.sin_family = AF_INET;
+	/*서버의 포트번호 대입*/
+	serverAddress.sin_port = htons(PORT);
+	if (connect(clientSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress)) == -1)
+	{
+		perror("Could not open data file");
+		//cout << "서버에 접속하는데 실패 했습니다 " << endl;
+		return ;
+	}
+	/*
+	char a[1];
+	recv(clientSocket, (char*)a, sizeof(a), 0);
+	a[0] = htonl(a[0]);
+	cout <<a<< endl;
+	*/
+	cout << "서버에 접속 했습니다 " << endl;
+	Sleep(3000);
+}
+
+void tank_c::con() {
+	for (int i = 0; i < 3; i++) {
+		coordinate[i] = htonl(coordinate[i]);
+	}
+	send(clientSocket, (char*)coordinate, sizeof(coordinate), 0);
+	for (int i = 0; i < 3; i++) {
+		coordinate[i] = ntohl(coordinate[i]);
+	}
+}
 
 void tank_c::move(void) {
 	while (1) {
 		if (kbhit()) {
 			tmp = _getch();
-			if(tmp == 224) tmp = _getch();
+			if (tmp == 224) tmp = _getch();
 			if (tmp == UP || tmp == DOWN || tmp == RIGHT || tmp == LEFT || tmp == STOP) {
 				key = tmp;
 			}
 		}
 		if (key == STOP) break;
 		switch (key) {
-		case UP: {
-				if (ty != 0 && map[ty - 1][tx] != 1) {
-					map[ty - 1][tx] = 4;
-					map[ty][tx] = 0;
-					ty--;
+		case UP: 
+			if (coordinate[0] != 0 && map[coordinate[0] - 1][coordinate[1]] != 1) {
+				map[coordinate[0] - 1][coordinate[1]] = 4;
+				map[coordinate[0]][coordinate[1]] = 0;
+				coordinate[0] -= 1;
+				coordinate[2] = 4;
+			}
+			break;
+		case DOWN: 
+			if (coordinate[0] != 9 && map[coordinate[0] + 1][coordinate[1]] != 1) {
+				map[coordinate[0] + 1][coordinate[1]] = 5;
+				map[coordinate[0]][coordinate[1]] = 0;
+				coordinate[0] += 1;
+				coordinate[2] = 5;
+			}
+			break;
+		case RIGHT: 
+			if (coordinate[1] != 9 && map[coordinate[0]][coordinate[1] + 1] != 1) {
+				map[coordinate[0]][coordinate[1] + 1] = 2;
+				map[coordinate[0]][coordinate[1]] = 0;
+				coordinate[1] += 1;
+				coordinate[2] = 2;
+			}
+			break;
+		case LEFT: 
+			if (coordinate[1] != 0 && map[coordinate[0]][coordinate[1] - 1] != 1) {
+				map[coordinate[0]][coordinate[1] - 1] = 3;
+				map[coordinate[0]][coordinate[1]] = 0;
+				coordinate[1] -= 1;
+				coordinate[2] = 3;
 			}
 			break;
 		}
-		case DOWN: {
-				if (ty != 9 && map[ty + 1][tx] != 1) {
-					map[ty + 1][tx] = 5;
-					map[ty][tx] = 0;
-					ty++;
-				}
-			break;
+		con();
+		print();
 		}
-		case RIGHT: {
-				if (tx != 9 && map[ty][tx + 1] != 1) {
-					map[ty][tx + 1] = 2;
-					map[ty][tx] = 0;
-					tx++;
-				}
-			break;
-		}
-		case LEFT: {
-				if (tx != 0 && map[ty][tx - 1] != 1) {
-					map[ty][tx - 1] = 3;
-					map[ty][tx] = 0;
-					tx--;
-				}
-			break;
-		}
-		}
-			print();
-			Sleep(500);
 	}
-}
+
 void tank_c::print() {
 	system("cls");
 	for (int i = 0; i < 10; i++) {
@@ -118,21 +176,24 @@ void tank_c::print() {
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 10; j++) {
 			if (i == 0) {
-				if (map[ty][j] == 0) cout << "  ";
-				if (map[ty][j] == 1) cout << "■";
-				if (map[ty][j] == 2) cout << "▶";
-				if (map[ty][j] == 3) cout << "◀";
-				if (map[ty][j] == 4) cout << "▲";
-				if (map[ty][j] == 5) cout << "▼";
+				if (map[coordinate[0]][j] == 0) cout << "  ";
+				if (map[coordinate[0]][j] == 1) cout << "■";
+				if (map[coordinate[0]][j] == 2) cout << "▶";
+				if (map[coordinate[0]][j] == 3) cout << "◀";
+				if (map[coordinate[0]][j] == 4) cout << "▲";
+				if (map[coordinate[0]][j] == 5) cout << "▼";
 			}
 			if (i == 1) cout << "□";
 		}
 		cout << endl;
 	}
-
+	Sleep(500);
 }
 void tank_c::set() {
+	Cli_St();
+	con();
 	print();
+	cout << "111" << endl;
 	while (1) {
 		move();
 	}
@@ -140,4 +201,6 @@ void tank_c::set() {
 int main() {
 	tank_c a;
 	a.set();
+
+	//a.set();
 }
